2776d06870bcfe9828d5710397e7a0de916988c0
Elijah Whitham-Powell <ew4@pdx.edu>
2016-07-07 14:57:13 -0700

    initial commit of project2 branch

--- src/test/java/edu/pdx/cs410J/ew4/TextDumperTest.java
+++ src/test/java/edu/pdx/cs410J/ew4/TextDumperTest.java
@@ -1,8 +1,14 @@
 package edu.pdx.cs410J.ew4;
 
+import org.junit.Before;
+
 /**
  * Created by edub629 on 7/7/16.
  */
 public class TextDumperTest {
-
+  @Before
+  public void setUp() throws Exception {
+    AppointmentBook testBook = new AppointmentBook("Evan");
+    Appointment testAppointment = new Appointment("Bang his own mother", "06/09/1993", "06/12/1993");
+  }
 }
