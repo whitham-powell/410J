8f024684c27c9504197c2adddec1e0a8e78b335d
Elijah Whitham-Powell <ew4@pdx.edu>
2016-07-07 14:30:15 -0700

    slight update of .gitignore

---
+++ src/main/java/edu/pdx/cs410J/ew4/TextDumper.java
@@ -0,0 +1,24 @@
+package edu.pdx.cs410J.ew4;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import edu.pdx.cs410J.AppointmentBookDumper;
+
+import java.io.IOException;
+
+/**
+ * Created by edub629 on 7/7/16.
+ */
+
+public class TextDumper implements AppointmentBookDumper {
+
+  /**
+   * Dumps an appointment book to some destination.
+   *
+   * @param book The appointment book whose contents are to be dumped
+   * @throws IOException Something went wrong while dumping the appointment book
+   */
+  @Override
+  public void dump(AbstractAppointmentBook book) throws IOException {
+
+  }
+}
---
+++ src/test/java/edu/pdx/cs410J/ew4/TextDumperTest.java
@@ -0,0 +1,8 @@
+package edu.pdx.cs410J.ew4;
+
+/**
+ * Created by edub629 on 7/7/16.
+ */
+public class TextDumperTest {
+
+}
