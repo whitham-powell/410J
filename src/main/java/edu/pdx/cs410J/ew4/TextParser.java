7ce82f578b9abc1fc11de839fbd9e05804febf8c
Elijah Whitham-Powell <ew4@pdx.edu>
2016-07-11 14:27:44 -0700

    Passing Project2IT without addition of TextParser and TextDumper

--- src/main/java/edu/pdx/cs410J/ew4/TextParser.java
+++ src/main/java/edu/pdx/cs410J/ew4/TextParser.java
@@ -5,21 +5,27 @@ import edu.pdx.cs410J.AppointmentBookParser;
 import edu.pdx.cs410J.ParserException;
 
 import java.io.*;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
 
-/**TODO document class
+/**
+ * TODO document class
  * Created by edub629 on 7/8/16.
  */
 public class TextParser implements AppointmentBookParser {
+  private String bookOwner;
   private boolean wasLoaded = false;
-  protected String fileName;
-  protected File file;
+  private String fileName;
+  private File file;
 
-  public TextParser(String fileName) {
+  public TextParser(String fileName, String bookOwner) {
     this.fileName = fileName;
     this.file = new File(fileName);
     if (this.file.exists()) {
       this.wasLoaded = true;
     }
+    this.bookOwner = bookOwner;
   }
 
   /**
@@ -30,21 +36,60 @@ public class TextParser implements AppointmentBookParser {
   @Override
   public AbstractAppointmentBook parse() throws ParserException {
     try (BufferedReader reader = new BufferedReader(new FileReader(this.file))) {
-      String line;
-      AppointmentBook theBook = new AppointmentBook(reader.readLine());
-      Appointment appointment = null;
+      String line = reader.readLine();
+      List<String> lineListing = reader.lines().collect(Collectors.toList());
+      ListIterator<String> iterator = lineListing.listIterator();
+      AppointmentBook theBook = new AppointmentBook(line);
+//      Appointment appointment = null;
       int infoCount = 0;
-      while ((line = reader.readLine()) != null) {
-        if (infoCount == 0) {
-          line = line.substring(1, line.length() - 1);
-          appointment = new Appointment();
-          appointment.setDescription(line);
-        } else if (infoCount == 1) {
-          appointment.setBeginTimeString(line);
-        } else if (infoCount == 2) {
-          appointment.setEndTimeString(line);
-          theBook.addAppointment(appointment);
+
+      // If the line count is not divisible by 3 throw ParserException
+      if (lineListing.size() % 3 != 0) {
+        throw new ParserException("Bad Formatting: Line count indicates missing data");
+      }
+
+
+      //TODO bad time formatting throw exception
+
+
+      String description;
+      String beginTimeString;
+      String endTimeString;
+      while (iterator.hasNext()) {
+        if (iterator.hasNext()) {
+          //TODO description missing quotes exception
+          description = iterator.next();
+          if (!description.startsWith("\"") || !description.endsWith("\"")) {
+            throw new ParserException("");
+          }
+          description = description.substring(1, description.length() - 1);
+        } else {
+          throw new ParserException("Hit end of file listing too soon - Expected an Appointment Description");
         }
+        if (iterator.hasNext()) {
+          beginTimeString = iterator.next();
+        } else {
+          throw new ParserException("Hit end of file listing too soon - Expected an Appointment Begin Time");
+        }
+        if (iterator.hasNext()) {
+          endTimeString = iterator.next();
+        } else {
+          throw new ParserException("Hit end of file listing too soon - Expected an Appointment End Time");
+        }
+
+        theBook.addAppointment(new Appointment(description, beginTimeString, endTimeString));
+      }
+      while ((line = reader.readLine()) != null) {
+//        if (infoCount == 0) {
+//          line = line.substring(1, line.length() - 1);
+//          appointment = new Appointment();
+//          appointment.setDescription(line);
+//        } else if (infoCount == 1) {
+//          appointment.setBeginTimeString(line);
+//        } else if (infoCount == 2) {
+//          appointment.setEndTimeString(line);
+//          theBook.addAppointment(appointment);
+//        }
         if (infoCount < 2) {
           ++infoCount;
         } else {
--- src/test/java/edu/pdx/cs410J/ew4/TextParserTest.java
+++ src/test/java/edu/pdx/cs410J/ew4/TextParserTest.java
@@ -5,10 +5,18 @@ import edu.pdx.cs410J.ParserException;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-/**TODO Document Test
+/**
+ * TODO Document Test
  * Created by edub629 on 7/8/16.
  */
 public class TextParserTest {
@@ -21,7 +29,7 @@ public class TextParserTest {
 
   @Test
   public void canLoadAnAppointmentBookFromAFile() {
-    TextParser textParser = new TextParser("test.txt");
+    TextParser textParser = new TextParser("test.txt", "Evan");
     AbstractAppointmentBook appointmentBook = null;
     String thisString = null;
     try {
@@ -31,7 +39,44 @@ public class TextParserTest {
     }
     assert appointmentBook != null;
     assertThat(appointmentBook.getOwnerName(), equalTo("Evan"));
+    assert appointmentBook.getAppointments() != null;
     assert !appointmentBook.getAppointments().isEmpty();
     assertThat(appointmentBook.getAppointments().toString(), equalTo(testBook.getAppointments().toString()));
+  }
+
+  @Test
+  public void badFormattingNotEnoughLinesOfTextInFile() {
+    try {
+      BufferedReader fr = new BufferedReader(new FileReader(new File("tooFewLinesTest.txt")));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    boolean caughtException;
+    TextParser textParser = new TextParser("tooFewLinesTest.txt", "Evan");
+    try {
+      AbstractAppointmentBook parsedBook = textParser.parse();
+      caughtException = false;
+    } catch (ParserException e) {
+      caughtException = true;
+      assertThat(e.getMessage(), containsString("Bad Formatting"));
+    }
+    assertThat(caughtException, is(true));
+  }
+
+  @Test
+  public void badFormattingMissingQuotesOnDescription() {
+    File testFile = new File("missingQuotesOnDescription.txt");
+    assertThat("Existence of test file", testFile.exists(), is(true));
+    assertThat("Can read test file", testFile.canRead(), is(true));
+    TextParser textParser = new TextParser("missingQuotesOnDescription.txt", "Evan");
+    boolean caughtException = false;
+    try {
+      AbstractAppointmentBook parsedBook = textParser.parse();
+    } catch (ParserException e) {
+      caughtException = true;
+      assertThat(e.getMessage(), containsString("Bad Formatting"));
+    }
+    assertThat("Exception was not caught", caughtException);
   }
 }
